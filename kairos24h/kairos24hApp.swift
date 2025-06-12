ce98037dddcc836e8baeba57bf62c9d147758f4b
juanlopezmarin <Juanteniente45@gmail.com>
2025-06-12 14:08:15 +0200

    Initial Commit

---
+++ kairos24h/kairos24hApp.swift
@@ -0,0 +1,32 @@
+//
+//  kairos24hApp.swift
+//  kairos24h
+//
+//  Created by Juan López Marín on 12/6/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct kairos24hApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ kairos24hTests/kairos24hTests.swift
@@ -0,0 +1,16 @@
+//
+//  kairos24hTests.swift
+//  kairos24hTests
+//
+//  Created by Juan López Marín on 12/6/25.
+//
+
+import Testing
+
+struct kairos24hTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
